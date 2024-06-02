f9930c7ecedb0aac2559526ff12c9951f86534ab
byteus <eximousdaddy@gmail.com>
2024-06-02 13:02:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ particlesJS('particles-js',
                 }
             },
             "size": {
-                "value": 3,
+                "value": 2.5,
                 "random": true,
                 "anim": {
                     "enable": false,
