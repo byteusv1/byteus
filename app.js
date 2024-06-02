b73986bc2232a743c6df4cb632fbdf0e98ba311d
byteus <eximousdaddy@gmail.com>
2024-06-02 13:05:19 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ particlesJS('particles-js',
     {
         "particles": {
             "number": {
-                "value": 500,
+                "value": 400,
                 "density": {
                     "enable": true,
                     "value_area": 800
